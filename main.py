7a61546341cfabe2a0c00a63b2a6878fac9bf83c
Joao Silva <jota.vasconceloss@gmail.com>
2024-04-05 19:27:17 +0000

    Adds table schema check

--- main.py
+++ main.py
@@ -7,22 +7,46 @@ from datetime import datetime
 from pathlib import Path
 
 class DataBase:
-    def __init__(self, database_file):
+    def __init__(self, database_file, table_name, man_fields):
+
+        # Class variables
+        self.table_name = table_name
+        self.man_fields = man_fields
+        self.conn = None
+        self.cursor = None
+
         try:
             if database_file.exists():
                 self.conn = sqlite3.connect(database_file)
                 self.cursor = self.conn.cursor()
                 print("Successfully connected to the database.")
+                if not self.__check_table_schema():
+                    print('Table schema does not have all mandatory fields')
+                    raise sqlite3.Error
             else:
-                print("Error - DB path is wrong: {database_file}")
+                print(f"Error - DB path is wrong: {database_file}")
+                raise sqlite3.Error
         except sqlite3.Error as e:
-            print("Error connecting to the database:", e)
+            print("Error connecting to the database.", e)
+            exit(1)
     
     def __del__(self):
-        self.cursor.close()
-        self.conn.close()
-        print("Database connection closed.")
+        if self.cursor:
+            self.cursor.close()
+            self.conn.close()
+            print("Database connection closed.")
 
+    def __check_table_schema(self):
+        ''' Check mandatory fields exist in the table
+        '''
+        schema = self.query(f"PRAGMA table_info({self.table_name})")
+        existing_fields = [field for schema_tuple in schema for field in self.man_fields if field in schema_tuple]
+
+        if sorted(self.man_fields) == sorted(existing_fields):
+            return True
+        else:
+            return False
+    
     def query(self, query):
         return self.cursor.execute(query).fetchall()
 
@@ -55,7 +79,9 @@ def previous_and_next(some_iterable):
 def get_commands(config, db_path):
 
     # Load database
-    db = DataBase(db_path)
+    db_name = config[0]['config']['database']['table_name']
+    mandatory_fields = config[0]['config']['database']['mandatory_fields']
+    db = DataBase(db_path, db_name, mandatory_fields)
 
     # Get generation config
     delay_mode = config[0]['config']['delay']['mode'].lower()
